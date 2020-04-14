c3d1b892d6758e2f39f4e501247291f594407adb
pietrodorto <s249615@studenti.polito.it>
2020-04-14 16:22:09 +0200

    dictionary

--- Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/model/Dictionary.java
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/model/Dictionary.java
@@ -1,19 +1,60 @@
 package it.polito.tdp.spellchecker.model;
 
+import java.io.*;
+import java.util.*;
+
 public class Dictionary {
-	String word;
 
-	public String getWord() {
-		return word;
-	}
-
-	public void setWord(String word) {
-		this.word = word;
-	}
+	List<String> dictionary;
+	String language;
 	
-	public loadDictionary(String Language) {
+
+	
+	public boolean loadDictionary(String language) {
 		
+		if (dictionary!=null && this.language.equals(language)){
+			return true;
+		}
+		
+		dictionary = new ArrayList<String>();
+		this.language = language;
+		
+		try {
+
+			FileReader fr = new FileReader("src/main/resources/" + language + ".txt");
+			BufferedReader br = new BufferedReader(fr);
+			String word;
+
+			while ((word = br.readLine()) != null) {
+				dictionary.add(word.toLowerCase());
+			}
+
+			Collections.sort(dictionary);
+
+			br.close();
+			System.out.println("Dizionario " + language + " loaded. Found " + dictionary.size() + " words.");
+			
+			return true;
+
+		} catch (IOException e) {
+			System.err.println("Errore nella lettura del file");
+			return false;
+		}
 	}
 	
+	public List<RichWord> spellCheckText(List<String> inputTextList){
+		List<RichWord> ltemp = new ArrayList<RichWord>();
+		
+		for(String parola: inputTextList) {
+			
+			RichWord richword = new RichWord(parola);
+			
+			if(dictionary.contains(parola)) {
+				
+			}
+		}
+			
+				
+	}
 
 }
