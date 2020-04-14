fb96d8ae195afae64390ae6b86eb70263469ebb6
pietrodorto <s249615@studenti.polito.it>
2020-04-14 10:49:27 +0200

    niente

--- Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -4,22 +4,49 @@
 
 package it.polito.tdp.spellchecker;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.awt.event.ActionEvent;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
 
 public class FXMLController {
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
-    private ResourceBundle resources;
+    @FXML
+    private ChoiceBox<?> btnChoice;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
-    private URL location;
+    @FXML
+    private TextArea txtnput;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
+    @FXML
+    private Button btnCheck;
+
+    @FXML
+    private TextArea txtWrongWords;
+
+    @FXML
+    private Text txtCountErrors;
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    private Text txtTimeCheck;
+    
+    
+
+    @FXML
+    void doClearText(ActionEvent event) {
 
     }
+
+    @FXML
+    void doSpellCheck(ActionEvent event) {
+
+    }
+
 }
 
 
