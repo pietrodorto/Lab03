0704d3bde16c1d7314c0a8a66a33dbf5c622b241
pietrodorto <s249615@studenti.polito.it>
2020-04-14 10:49:39 +0200

    ffffffh  hgf

---
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/model/Dictionary.java
@@ -0,0 +1,15 @@
+package it.polito.tdp.spellchecker.model;
+
+public class Dictionary {
+	String word;
+
+	public String getWord() {
+		return word;
+	}
+
+	public void setWord(String word) {
+		this.word = word;
+	}
+	
+
+}
